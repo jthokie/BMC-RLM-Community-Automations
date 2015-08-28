387d6f76b0185af5340d8ac2b0d5ec993da9b6d7
jthokie <jthokie@gmail.com>
2015-08-28 16:57:56 -0400

    Correcting copyright for community based content

--- rpm/jboss/enterprise-application-platform/jboss-eap-deploy-linux.sh
+++ rpm/jboss/enterprise-application-platform/jboss-eap-deploy-linux.sh
@@ -1,9 +1,17 @@
 #############################################################################
-# Copyright @ 2012-2015 BMC Software, Inc.                                  #
-# This script is supplied as a template for performing the defined actions  #
-# via the BMC Release Process Management. This script is written        #
-# to perform in most environments but may require changes to work correctly #
-# in your specific environment.                                             #
+# Copyright [2015] [Jon Thomas]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 #############################################################################
 #
 # executes jboss-cli with deploy command
